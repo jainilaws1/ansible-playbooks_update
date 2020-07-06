d16d32b171abdb076c186667086946f588ad57d7
jainilaws1 <61342592+jainilaws1@users.noreply.github.com>
2020-07-06 18:46:34 +0530

    Update modify_bosh_yml.sh

--- modify_bosh_yml.sh
+++ modify_bosh_yml.sh
@@ -9,18 +9,6 @@ fi
 echo $1
 file_name=$1
 echo $file_name
-sudo apt-get update -y
-sudo apt-get install awscli -y
-mkdir -p /root/.aws
-export AWS_ACCESS_KEY_ID=AKIA6ONPWXEEOCX6UNZO
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_REGION=us-west-2
-
-echo "[default]" > /root/.aws/credentials
-echo "aws_access_key_id = "$AWS_ACCESS_KEY_ID >> /root/.aws/credentials
-echo "aws_secret_access_key = "$AWS_SECRET_ACCESS_KEY >> /root/.aws/credentials
-echo "[default]" > /root/.aws/config
-echo "region = "$AWS_REGION >>/root/.aws/config
 group_id=$(cat ${file_name} | grep default_security_groups | cut -d ":" -f2 | sed "s/[]//g" | sed "s/\"//g")
 group_name=$(aws ec2 describe-security-groups ${group_id} | grep GroupName | cut -d ':' -f2 | sed "s/\"//g")
 cat ${file_name} | sed "s/${group_id}/${group_name}/g" > ${file_name}_update
