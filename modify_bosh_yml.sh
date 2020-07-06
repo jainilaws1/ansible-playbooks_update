2002bd2d830023f1ba9d04b8f798223ceca4e294
jainilaws1 <61342592+jainilaws1@users.noreply.github.com>
2020-07-06 12:38:33 +0530

    Update modify_bosh_yml.sh

--- modify_bosh_yml.sh
+++ modify_bosh_yml.sh
@@ -1,5 +1,13 @@
 #script to download awscli and change the security id in bosh.yml to security group name.
-group_id=$(cat bosh.yml | grep default_security_groups | cut -d "=" -f2)
+if [ -z "$1" ]
+then
+#{
+	echo "ERROR: File name not passed"
+	exit 1
+#}
+fi
+file_name=$1
+group_id=$(cat ${file_name} | grep default_security_groups | cut -d "=" -f2)
 group_name=$(aws ec2 describe-security-groups ${group_id} | grep GroupName | cut -d ':' -f2 | sed "s/\"//g")
-cat bosh.yml | sed "s/${group_id}/${group_name}/g" > bosh_update.yml
-mv bosh_update.yml bosh.yml
+cat ${file_name} | sed "s/${group_id}/${group_name}/g" > ${file_name}_update
+mv ${file_name}_update ${file_name}
